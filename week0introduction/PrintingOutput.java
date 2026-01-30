5d8f94fc8851a0fc536bb8433e65784820b2a4ef
Aya <alharazinaya@gmail.com>
2026-01-30 17:10:30 +0200

    edit week 0

--- week0introduction/PrintingOutput.java
+++ week0introduction/PrintingOutput.java
@@ -36,22 +36,7 @@ public class PrintingOutput {
         */
         
         
-        
-        
-        
-                int n = 5;
 
-        for (int i = 1; i <= n; i++) {
-            // spaces
-            for (int s = 1; s <= n - i; s++) {
-                System.out.print(" ");
-            }
-            // stars
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
          
     }
 }
